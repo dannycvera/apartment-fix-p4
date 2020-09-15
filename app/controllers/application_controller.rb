a63f559e1f4e29907a0951cc9bfd36cb79a9ddba
danv <dannyvera@gmail.com>
2020-09-15 18:53:34 -0400

    rails user auth complete but needs testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,17 @@ class ApplicationController < ActionController::API
       render json: @user.errors, status: :unprocessable_entity
     end
   end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = decode(header)
+      @current_user = User.find(@decoded[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
