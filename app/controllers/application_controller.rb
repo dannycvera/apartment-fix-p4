a326c09e88ea5fbe0989e59a3dc00908a151fa97
danv <dannyvera@gmail.com>
2020-09-15 17:24:28 -0400

    JSON web tokens in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new decoded
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except('password_digest'),
+        token: @token
+      }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 end
