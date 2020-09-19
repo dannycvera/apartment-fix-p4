c031cbbcc8e6fed87ae88e5050731ec7ade675b8
danv <dannyvera@gmail.com>
2020-09-19 10:48:44 -0400

    added image error checking to home page

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -3,15 +3,31 @@ import "./Home.css";
 function Home(props) {
   const { issues } = props;
 
+  const defaultSrc = (e) => {
+    e.target.src = require("../img/noImage.svg");
+  };
+
   const issueImages = issues.map((issue) => {
     if (issue.image_url.slice(-3) === "mp4") {
       return (
-        <video className="home-img" key={issue.id} controls>
+        <video
+          className="home-img"
+          key={issue.id}
+          controls
+          onError={defaultSrc}
+        >
           <source src={issue.image_url} type="video/mp4" />
         </video>
       );
     } else {
-      return <img className="home-img" key={issue.id} src={issue.image_url} />;
+      return (
+        <img
+          className="home-img"
+          key={issue.id}
+          src={issue.image_url}
+          onError={defaultSrc}
+        />
+      );
     }
   });
 
