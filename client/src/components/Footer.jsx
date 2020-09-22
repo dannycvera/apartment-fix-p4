0ba639bc82c3a10539d3f87d13f324c4be962e97
danv <dannyvera@gmail.com>
2020-09-22 04:18:38 -0400

    social links

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="social-bar">
+      <a
+        href="https://github.com/dannycvera"
+        className="github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="github-icon"
+          alt="Daniel Vera's Github"
+          src={require("../img/GitHub-logo-32px.png")}
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/daniel-vera-65bbb07/"
+        className="linkedin"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="linkedin-icon"
+          alt="Daniel Vera's LinkedIn"
+          src={require("../img/linkedIn-logo.png")}
+        />
+      </a>
+      <p>site by Daniel Vera</p>
+    </div>
+  );
+}
+
+export default Footer;
--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./Layout.css";
 
 export default function Layout(props) {
@@ -10,6 +11,8 @@ export default function Layout(props) {
         handleLogout={props.handleLogout}
       />
       <main>{props.children}</main>
+
+      <Footer></Footer>
     </>
   );
 }
